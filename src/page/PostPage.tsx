f2b72e6ddb1f98ced4b19957169a7f8b4390c4ba
0xtusharganotra <ganotra.vox@gmail.com>
2025-09-05 08:33:07 +0530

    remove unused imports

--- src/page/PostPage.tsx
+++ src/page/PostPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
